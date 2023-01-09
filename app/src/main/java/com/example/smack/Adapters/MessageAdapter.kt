ff8da41d815db08f3ca31615f0cd81a739da9073
Yash Mimani <yashmimani3107@gmail.com>
2023-01-09 23:53:32 +0530

    send and request data and also display in UI

---
+++ app/src/main/java/com/example/smack/Adapters/MessageAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.smack.Adapters
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smack.Model.Message
+import com.example.smack.R
+import com.example.smack.Services.UserDataService
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by jonnyb on 9/10/17.
+ */
+class MessageAdapter(val context: Context, val messages: ArrayList<Message>) :  RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder?.bindMessage(context, messages[position])
+    }
+
+    override fun getItemCount(): Int {
+        return messages.count()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.message_list_view, parent, false)
+        return ViewHolder(view)
+    }
+
+
+    inner class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val userImage = itemView?.findViewById<ImageView>(R.id.messageUserImage)
+        val timeStamp = itemView?.findViewById<TextView>(R.id.timestampLbl)
+        val userName = itemView?.findViewById<TextView>(R.id.messageUserNameLbl)
+        val messageBody = itemView?.findViewById<TextView>(R.id.messageBodyLbl)
+
+        fun bindMessage(context: Context, message: Message) {
+            val resourceId = context.resources.getIdentifier(message.userAvatar, "drawable", context.packageName)
+            userImage?.setImageResource(resourceId)
+            userImage?.setBackgroundColor(UserDataService.returnAvatarColor(message.userAvatarColor))
+            userName?.text = message.userName
+            timeStamp?.text = returnDateString(message.timeStamp)
+            messageBody?.text = message.message
+        }
+
+        fun returnDateString(isoString: String) : String {
+            val isoFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+            isoFormatter.timeZone = TimeZone.getTimeZone("UTC")
+            var convertedDate = Date()
+            try {
+                convertedDate = isoFormatter.parse(isoString)
+            } catch (e: ParseException) {
+                Log.d("PARSE", "Cannot parse date")
+            }
+
+            val outDateString = SimpleDateFormat("E, h:mm a", Locale.getDefault())
+            return outDateString.format(convertedDate)
+        }
+    }
+}
